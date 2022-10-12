ed3087f7cbaa48e1790f78b3a9841087965f9ed6
Joz-Bruer Quince <quincejozbruer@gmail.com>
2022-10-11 23:30:47 -0700

    Adding the assets and Html layouts

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+// Variable for my different operating system
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
 /**
  * Create the main function window to launch the application
  */
@@ -9,12 +13,25 @@ const createMainWindow = () => {
 	const mainWindow = new BrowserWindow({
 		title: 'Image Resizer',
 		height: 600,
-		width: 500,
+		width: isDev ? 1000 : 500,
 	});
 
+	//Open dev tools if on dev env
+	if (isDev) {
+		mainWindow.webContents.openDevTools();
+	}
 	mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 };
 
 app.whenReady().then(() => {
 	createMainWindow();
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+	});
+});
+
+// close the application
+
+app.on('window-all-closed', () => {
+	if (!isMac) app.quit();
 });
---
+++ renderer/js/script.js
@@ -0,0 +1,1 @@
+// Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
